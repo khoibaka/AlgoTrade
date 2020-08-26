454eb853ece44c681e569b3b37d89a042c3df706
Khoi Le <khoibaka@gmail.com>
2020-08-25 20:47:04 -0400

    wrote 2 converter from date string to ms and interval to ms

--- src/utils/time_converter.py
+++ src/utils/time_converter.py
@@ -1,4 +1,6 @@
-
+import pytz
+import dateparser
+from datetime import datetime
 
 
 class TimeConverter:
@@ -13,22 +15,42 @@ class TimeConverter:
     @staticmethod
     def interval_to_ms(interval):
 
-        if not isinstance(interval, str) or len(interval) == 0:
-            return None
+        if not isinstance(interval, str) :
+            raise ValueError('Interval should be string')
+
+        if len(interval) == 0:
+            raise ValueError('Length of interval should be larger than 0')
 
         unit = interval[-1]
         if not TimeConverter.seconds_per_unit.get(unit):
-            return None
+            raise ValueError('Interval trail should be in these values m,h,d,w')
 
         try:
             ms = int(interval[:-1])
             ms *= TimeConverter.seconds_per_unit.get(unit) * TimeConverter.ms_per_second
             return ms
         except ValueError:
-            return None
+            raise ValueError('Error with the interval value')
 
-def test():
-    print(TimeConverter.interval_to_ms('2w'))
+    @staticmethod
+    def date_to_ms(date_str):
+
+        if not isinstance(date_str, str):
+            raise ValueError('date_str should be string')
+
+        epoch = datetime.utcfromtimestamp(0).replace(tzinfo=pytz.utc)
+        d = dateparser.parse(date_str)
+
+        if not d:
+            raise ValueError('date_str is not in correct format')
+
+        if d.tzinfo is None or d.tzinfo.utcoffset(d) is None:
+            d = d.replace(tzinfo=pytz.utc)
+
+        return int((d - epoch).total_seconds() * TimeConverter.ms_per_second)
+
+
+
 
 if __name__ == '__main__':
-    test()
+    print(TimeConverter.date_to_ms('13-12-2019'))
--- src/utils/time_converter_test.py
+++ src/utils/time_converter_test.py
@@ -2,9 +2,17 @@ import unittest
 from src.utils.time_converter import *
 
 class TimeConverterTest(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_interval_to_ms(self):
+        self.assertRaises(ValueError, TimeConverter.interval_to_ms, None)
+        self.assertRaises(ValueError, TimeConverter.interval_to_ms, '2')
+        self.assertRaises(ValueError, TimeConverter.interval_to_ms, 'w')
+        self.assertRaises(ValueError, TimeConverter.interval_to_ms, '2b3w')
+        self.assertEqual(TimeConverter.interval_to_ms('2w'), 2 * 60 * 60 * 1000 * 24 * 7)
 
+    def test_date_to_ms(self):
+        self.assertRaises(ValueError, TimeConverter.date_to_ms, None)
+        self.assertRaises(ValueError, TimeConverter.date_to_ms, '13-13-2019')
+        self.assertEqual(TimeConverter.date_to_ms('13-12-2019'), 1576195200000)
 
 if __name__ == '__main__':
     unittest.main()
